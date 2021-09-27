d572b49188060c1920c3bfff5be4816e19d15f8f
Daphne <daphne0029@hotmail.com>
2021-09-26 17:20:00 -0700

    Task2.3

--- public-api/app/Exceptions/Handler.php
+++ public-api/app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Client\RequestException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -50,6 +51,11 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
+        if ($exception instanceof RequestException) {
+            return response()->json([
+                'error' => 'Resource not found'
+            ], 500);
+        }
         return parent::render($request, $exception);
     }
 }
--- public-api/app/Http/Controllers/UserFoodController.php
+++ public-api/app/Http/Controllers/UserFoodController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Http\Request;
+use Exception;
 
 class UserFoodController extends Controller
 {
@@ -18,18 +19,21 @@ class UserFoodController extends Controller
     public function foods(int $userId)
     {
         $response = Http::get("{$this->privateApiUrl}/users/{$userId}/foods");
+        $response->throw();
         return $response->json();
     }
 
     public function food(int $userId, int $foodId)
     {
         $response = Http::get("{$this->privateApiUrl}/users/{$userId}/foods/{$foodId}");
+        $response->throw();
         return $response->json();
     }
 
     public function deleteFood(int $userId, int $foodId)
     {
         $response = Http::delete("{$this->privateApiUrl}/users/{$userId}/foods/{$foodId}");
+        $response->throw();
         return $response->json();
     }
 
@@ -40,6 +44,7 @@ class UserFoodController extends Controller
         $response = Http::put("{$this->privateApiUrl}/users/{$userId}/foods/{$foodId}", [
             'servingsPerWeek' => $servingsPerWeek
         ]);
+        $response->throw();
         return $response->json();
     }
 }
